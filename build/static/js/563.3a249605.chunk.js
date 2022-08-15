a08f83b799b1427d843a5f6f75edc36276efaeab
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-15 20:23:16 +0900

    feat: Init compress-create-react-app

---
+++ build/static/js/563.3a249605.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[563],{2563:function(n,e,c){c.r(e);c(2791);var i=c(184);e.default=function(){return(0,i.jsx)("div",{children:"Loading"})}}}]);
+//# sourceMappingURL=563.3a249605.chunk.js.map
