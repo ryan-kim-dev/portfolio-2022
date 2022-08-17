b1af9c3c53cbf1bde3ffb3f4be6e1d0137ccfaec
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-17 16:18:54 +0900

    feat: Add nodemailer w/ iframe

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SectionTitle from '../components/common/SectionTitle';
 import { Container } from '../GlobalStyle';
 import styled from 'styled-components';
 import ContactLinks from '../components/common/ContactLinks';
+import Iframe from 'react-iframe';
+
 // import axios from 'axios';
 // import { axiosInstance } from '../config';
 const Contact = () => {
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
-  const onChange = e => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setForm(prev => ({
-      ...prev, // 기존 객체 복사 (spread)
-      [name]: value,
-      // input에 부여된 name 속성의 값을 key로, name 키를 가진 값을 value로 설정.
-      // [] 로 감싸 key 값을 동적으로 받는다.
-      // 참고 : https://kjhg478.tistory.com/27
-    }));
-  };
-
-  const onSubmit = async e => {
-    e.preventDefault();
-    fetch('/mail', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ ...form, secret: 'firebaseIsCool' }),
-    })
-      .then(res => {
-        console.log(res.form);
-      })
-      .catch(err => alert(`${err}`));
-  };
-
   return (
     <ContactContainer id="contact">
       <SectionTitle title={'Contact'} />
@@ -56,40 +24,15 @@ const Contact = () => {
           </div>
           <ContactLinks />
         </SocialTab>
-        <Form
-          action="/mail"
-          method="post"
-          onChange={onChange}
-          onSubmit={onSubmit}
-        >
-          <Div>
-            <Label htmlFor="name">Name</Label>
-            <Input type="text" name="name" id="name" placeholder="성함" />
-          </Div>
-          <Div>
-            <Label htmlFor="email">Address</Label>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="메일주소"
-            />
-          </Div>
-          <Div>
-            <Label htmlFor="subject">Title</Label>
-            <Input type="text" name="subject" id="subject" placeholder="제목" />
-          </Div>
-          <Div>
-            <Label htmlFor="message">Message</Label>
-            <textarea
-              name="message"
-              id="message"
-              required
-              placeholder="본문을 입력해주세요"
-            />
-          </Div>
-          <Input type="submit" className="submit-btn" value="Send Email" />
-        </Form>
+        <Iframe
+          url="https://micro-app-mailer.herokuapp.com/"
+          width="800px"
+          height="max-content"
+          id="myId"
+          className="myClassname"
+          display="initial"
+          position="relative"
+        />
       </FormContainer>
     </ContactContainer>
   );
@@ -137,58 +80,4 @@ const SubTitle = styled.div`
 const Img = styled.img`
   width: 50%;
   border-radius: 50%;
-`;
-
-const Form = styled.form`
-  background: rgba(255, 255, 255, 0.2);
-  width: 70%;
-  display: flex;
-  row-gap: 30px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  position: relative;
-  padding: 1rem;
-
-  textarea {
-    background: inherit;
-    border-top: none;
-    border-left: none;
-    border-right: none;
-    border-bottom: 1.5px solid #414141;
-    width: 400px;
-    height: 150px;
-  }
-`;
-
-const Div = styled.div``;
-
-const Label = styled.label`
-  display: block;
-  font-family: 'Oswald', sans-serif;
-`;
-
-const Input = styled.input`
-  background: inherit;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-bottom: 1.5px solid #414141;
-  width: 400px;
-
-  &.submit-btn {
-    width: max-content;
-    cursor: pointer;
-    font-family: 'Oswald', sans-serif;
-    color: inherit;
-    text-decoration: none;
-    border: 2px solid #414141;
-    padding: 2px 10px;
-    background-color: transparent;
-
-    :hover {
-      color: #bebebe;
-      border: 2px solid #bebebe;
-    }
-  }
 `;
