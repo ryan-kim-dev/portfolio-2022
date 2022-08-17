7b8e35560d155b6752881be4ebfba08128387b6a
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-17 13:51:07 +0900

    chore: Deploy firebase functions

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import SectionTitle from '../components/common/SectionTitle';
+// import SectionTitle from '../components/common/SectionTitle';
 import { Container } from '../GlobalStyle';
 import Row from '../components/works/Row';
 
 const Works = () => {
-  const [projects, setProjects] = useState([
-    // 더미데이터. 추후 파이어베이스 db 연동으로 변경예정
-    {
-      name: '',
-      id: '',
-      photoURL: '',
-    },
-  ]);
+  // const [projects, setProjects] = useState([
+  //   // 더미데이터. 추후 파이어베이스 db 연동으로 변경예정
+  //   {
+  //     name: '',
+  //     id: '',
+  //     photoURL: '',
+  //   },
+  // ]);
   // console.log(setProjects);
   return (
     <Container>
       <WorksWrapper>
-        <SectionTitle title={'works'} projects={projects} />
+        {/* <SectionTitle title={'works'} projects={projects} /> */}
         <Row title="TEAM PROJECTS"></Row>
         <Row title="PERSONAL PROJECTS"></Row>
       </WorksWrapper>
