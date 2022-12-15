ea89fd5837c008d9867dbc744456bf15e3a92483
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-15 18:11:09 +0900

    chore: 빌드 태스크 명령어 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,12 @@
 const {
   override,
   addBabelPlugin,
-  addWebpackPlugin,
   addWebpackModuleRule,
 } = require('customize-cra');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = override(
   addBabelPlugin('babel-plugin-styled-components'),
-  addWebpackPlugin(new BundleAnalyzerPlugin()),
+
   addWebpackModuleRule({
     test: /\.splinecode$/,
     use: [
