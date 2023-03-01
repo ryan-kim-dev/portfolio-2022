cb48ef85595e9a6e89c933b87480b82ab3cd7e86
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-03-01 20:32:56 +0900

    Chore: Install packages for notion integration & tailwind css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../styles/global-style';
-import theme from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,10 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Ryan Kim&apos;s portfolio</title>
       </Head>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />;
-      </ThemeProvider>
+      <Component {...pageProps} />;
     </>
   );
 }
